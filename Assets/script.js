6b8759ed10128b132298818ff15717cdf0fc90cc
Daniel Bradley <danielbvideo@gmail.com>
2021-07-13 11:13:17 -0400

    Push of screenshots, edited CSS, Script, Index, and README

--- Assets/script.js
+++ Assets/script.js
@@ -1,65 +1,81 @@
-// Date will pull in automatically
+// Date will pull in automatically using Moment
 var now = moment().format("[Today is] dddd, MMMM Do, YYYY");
 $("#currentDay").text(now);
 console.log(now);
 
-
-
-// local storage of inputted text. make local storage last for 24 hours if possibleto correspond with daily planner aspect
-
-var typedMessage9 = $("#9");
-
 $(document).ready(function(){
+// these are the local storage data identifiers, defined below. These are "getting" items from local storage on page load
+  $('#9').html(window.localStorage.ninedata)
+  $('#10').html(window.localStorage.tendata);
+  $('#11').html(window.localStorage.elevendata);
+  $('#12').html(window.localStorage.twelvendata);
+  $('#13').html(window.localStorage.thirteendata);
+  $('#14').html(window.localStorage.fourteendata);
+  $('#15').html(window.localStorage.fifteendata);
+  $('#16').html(window.localStorage.sixteendata);
+  $('#17').html(window.localStorage.seventeendata);
 
-$(".saveBtn").on("click",function (){
-  var divID = $(this).attr("data-id")
+// assignment of variables for the function for the click
+  $(".saveBtn").on("click",function (){
+  var typedMessage9 = $('#9').text();
+  var typedMessage10 = $('#10').text();
+  var typedMessage11 = $('#11').text();
+  var typedMessage12 = $('#12').text();
+  var typedMessage13 = $('#13').text();
+  var typedMessage14 = $('#14').text();
+  var typedMessage15 = $('#15').text();
+  var typedMessage16 = $('#16').text();
+  var typedMessage17 = $('#17').text();
 
-
-  var typedMessage9Array = JSON.parse(localStorage.getItem(typedMessage9)) || []
-
-  console.log( $(typedMessage9 + divID).text())
-
-  var displayString = typedMessage9.text;
-
-  typedMessage9Array.push(displayString)
-  localStorage.setItem(typedMessage9, JSON.stringify(typedMessage9Array))
-  console.log(localStorage);
+// setting local storage with a key and a value
+  localStorage.setItem("9:00", typedMessage9);
+  localStorage.setItem("10:00", typedMessage10);
+  localStorage.setItem("11:00", typedMessage11);
+  localStorage.setItem("12:00", typedMessage12);
+  localStorage.setItem("13:00", typedMessage13);
+  localStorage.setItem("14:00", typedMessage14);
+  localStorage.setItem("15:00", typedMessage15);
+  localStorage.setItem("16:00", typedMessage16);
+  localStorage.setItem("17:00", typedMessage17);
+  console.log(localStorage)
+        
+//this is assigning local storage with a value
+  localStorage.ninedata = typedMessage9;
+  localStorage.tendata = typedMessage10;
+  localStorage.elevendata = typedMessage11;
+  localStorage.twelvendata = typedMessage12;
+  localStorage.thirteendata = typedMessage13;
+  localStorage.fourteendata = typedMessage14;
+  localStorage.fifteendata = typedMessage15;
+  localStorage.sixteendata = typedMessage16;
+  localStorage.seventeendata = typedMessage17;
+  })
   
-  for (let index = 0; index < typedMessage9Array.length; index++) {
-    const displayString = typedMessage9Array[index];
-    
-  }
-
-
-
- })
-
-});
-
+  });
 // end of local storage
 
 
-
-
-
-
-
-// this function takes the current hour from the date function and parses he information to look for a numerical value in the colorcode div
+// this function takes the current hour from the date function and parses he in the corresponding colorcode div
 // based on the time parameters of the parsed information, it will determine the timeframe for each color change 
 var currentHour = (new Date()).getHours();
 $('.colorcode')
   .each(function(){
     var val = parseInt($(this).prop('id'));
-    if(val > currentHour && val < currentHour + 8){
+    if(val > currentHour && val < currentHour + 9){
       $(this).css('background-color', '#77dd77');
-    }else if(val < currentHour && val > currentHour-15){
+    }else if(val < currentHour && val > currentHour-14){
       $(this).css('background-color', '#d3d3d3');
     }else if(val === currentHour){
       $(this).css('background-color','#ff6961');
     }else{
-      $(this).css('background-color','White');
+      $(this).css('background-color','white');
     }
   });
 
 
-
+// This will clear local storage and wipe the page of contents so that user can enter new notes on a new day
+$("#eraseButton").on("click",function () {
+    localStorage.clear();
+    location.reload();
+    });
+  
--- README.md
+++ README.md
@@ -1,15 +1,12 @@
-```md
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours
-WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-WHEN I click into a timeblock
-THEN I can enter an event
-WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
-```
+This application is a basic day planner. Set around the standard 9:00 AM to 5:00 PM, it allows for users to record their tasks for the day - according to the hour that they would like to accomplish it. 
+
+There is also a color-coded system, based on the time of the day it is. If it is in the past (the hour has passed), the block color will be gray. If it is the current hour, the block color will be red. If it is the future (time of day yet to come), the color will be green. The text color for each is visible regardless of background color. 
+
+Upon clicking the disk icon to the right of each text box,the task can be saved and written to local storage and will re-appear upon page reload. There is also an "Erase Notes" button at the bottom which clears the local storage. "Erase Notes" was not a requirement, however for ease of use this was added as an extra feature.
+
+
+Application Repository Link: 
+Deployed Application Link:
+
+![Image 1 of application:]()
+![Image 2 of application:]()
